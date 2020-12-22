6563ca8fdfe232c72b984d2f43deb1f87f6fd7ef
chaeso7279 <chaeso7279@gmail.com>
2020-12-22 11:59:51 +0900

    프론트 합치기전 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,35 @@
-import React, { Component } from "react";
-import MyNFTContract from "./contracts/MyNFT.json";
-import AuctionsContract from "./contracts/Auctions.json";
-import getWeb3 from "./getWeb3";
+import logo from './logo.svg';
+import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import Main from './routes/Main';
+import MyShareList from './routes/MyShareList';
+import MyPage from'./routes/MyPage';
+import MileageApplication from'./routes/MileageApplication';
+import ShareItemList from './routes/ShareItemList';
+import ShareItemDetail from './routes/ShareItemDetail';
+import ShareTalentList from './routes/ShareTalentList';
+import ShareTalentDetail from './routes/ShareTalentDetail';
+import Navigation from './components/Navigation';
 
-import "./App.css";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      web3: null
-    };
+function App() {
+  let router;
+  const navigate = (pageName = '') =>{
+    router.history.push('/'+pageName);
   }
 
-  componentWillMount() {
-    getWeb3
-      .then(results => {
-        this.setState({
-          web3: results.web3
-        });
+  return (
+      <HashRouter ref = {(r)=>{router = r;}}>
+        <Route path="/" exact={true} component={Main}/>
+        <Route path="/mysharelist" component={MyShareList}/>
+        <Route path="/mileage_application" component={MileageApplication}/>
+        <Route path="/mypage" component={MyPage}/>
 
-        this.instantiateContract();
-      })
-      .catch(() => {
-        console.log("Error finding web3.");
-      });
-  }
-
-  instantiateContract() {
-    const contract = require("truffle-contract");
-    const MyNFT = contract(MyNFTContract);
-    const Auctions = contract(AuctionsContract);
-    
-    MyNFT.setProvider(this.state.web3.currentProvider);
-    Auctions.setProvider(this.state.web3.currentProvider);
-    
-  }
-
-  render() {
-    return <div className="App">Fruit shop</div>;
-  }
+        <Route path="/share_item_list" component={ShareItemList}/>
+        <Route path="/share_item_detail" component={ShareItemDetail}/>
+        <Route path="/share_talent_list" component={ShareTalentList}/>
+        <Route path="/share_talent_detail" component={ShareTalentDetail}/>
+      </HashRouter>
+  );
 }
+
+export default App;
