59ff6ef5bd3b3dd9728b61539570b380f74ad89c
Jeongum <aldlswjddma@naver.com>
2020-12-22 11:10:03 +0900

    fix structure / add cancel function

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,44 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import MyNFTContract from "./contracts/MyNFT.json";
+import AuctionsContract from "./contracts/Auctions.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  constructor(props) {
+    super(props);
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+    this.state = {
+      web3: null
+    };
+  }
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+  componentWillMount() {
+    getWeb3
+      .then(results => {
+        this.setState({
+          web3: results.web3
+        });
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+        this.instantiateContract();
+      })
+      .catch(() => {
+        console.log("Error finding web3.");
+      });
+  }
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  instantiateContract() {
+    const contract = require("truffle-contract");
+    const MyNFT = contract(MyNFTContract);
+    const Auctions = contract(AuctionsContract);
+    
+    MyNFT.setProvider(this.state.web3.currentProvider);
+    Auctions.setProvider(this.state.web3.currentProvider);
+    
+  }
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
+    return <div className="App">Fruit shop</div>;
   }
 }
-
-export default App;
